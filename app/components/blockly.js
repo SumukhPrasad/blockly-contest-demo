055cea3fa44b1c0bfdbc3d71e718e861d0eddcaf
Sumukh Prasad <sumukhprasad@icloud.com>
2023-07-17 07:34:46 +0530

    Added blockly configuration.

--- app/components/blockly.js
+++ app/components/blockly.js
@@ -40,12 +40,36 @@ function BlocklyEditor() {
          ]
        }
      ]
+   };
+
+   let workspaceConfiguration = {collapse : false, 
+    comments : true, 
+    disable : true, 
+    maxBlocks : Infinity, 
+    trashcan : true, 
+    horizontalLayout : true, 
+    toolboxPosition : 'end',
+    grid : {
+      spacing : 20, 
+      length : -30, 
+      colour : '#555', 
+      snap : false
+    }, 
+    zoom : {
+      controls : true, 
+      wheel : true, 
+      startScale : 1, 
+      maxScale : 3, 
+      minScale : 0.3, 
+      scaleSpeed : 1.2
+    }
    }
 
   return (
     <BlocklyWorkspace
       className={styles.editor} // you can use whatever classes are appropriate for your app's CSS
       toolboxConfiguration={toolboxConfiguration} // this must be a JSON toolbox definition
+      workspaceConfiguration={workspaceConfiguration}
     />
   )
 }
